f2bd759c55b9edac292d3fdb7affcd3dc5f40b7c
Nick Hoath <nick.hoath@gmail.com>
2020-02-28 13:50:31 +0000

    Building & running JNI test to UWP Including MS dlls required

--- javelin_test/src/javelin_test/javelin_test.java
+++ javelin_test/src/javelin_test/javelin_test.java
@@ -10,6 +10,21 @@ public class javelin_test {
 	
 	public static void main(String[] args)
 	{
+		System.loadLibrary("msvcp140d_app");
+		System.loadLibrary("vcruntime140_1d_app");
+		System.loadLibrary("VCRUNTIME140D_APP");
+		System.loadLibrary("ucrtbased");
+		System.loadLibrary("api-ms-win-core-processthreads-l1-1-0");
+		System.loadLibrary("api-ms-win-core-debug-l1-1-0");
+		System.loadLibrary("api-ms-win-core-errorhandling-l1-1-0");
+		System.loadLibrary("api-ms-win-core-string-l1-1-0");
+		System.loadLibrary("api-ms-win-core-profile-l1-1-0");
+		System.loadLibrary("api-ms-win-core-sysinfo-l1-1-0");
+		System.loadLibrary("api-ms-win-core-interlocked-l1-1-0");
+		System.loadLibrary("api-ms-win-core-winrt-l1-1-0");
+		System.loadLibrary("api-ms-win-core-heap-l1-1-0");
+		System.loadLibrary("api-ms-win-core-memory-l1-1-0");
+		System.loadLibrary("api-ms-win-core-libraryloader-l1-2-0");
 		System.loadLibrary("javelin");
 		m_jt = new javelin_test();
 	}
